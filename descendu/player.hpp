2722cb9d2a3a80147d29e103c535f2afdde7c0ba
Rhys Ulerich <rhys.ulerich@gmail.com>
2016-12-12 21:54:09 -0500

    Stub player + tests

---
+++ descendu/player.hpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016 Rhys Ulerich
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef DESCENDU_PLAYER_H
+#define DESCENDU_PLAYER_H
+
+namespace descendu {
+
+class player
+{
+public:
+
+};
+
+} // namespace
+
+#endif
