a0290178a0341ac93d63153de3f421baf453dd54
Rhys Ulerich <rhys.ulerich@gmail.com>
2016-12-12 22:57:55 -0500

    Add resource + test

---
+++ descendu/resource.hpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016 Rhys Ulerich
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef DESCENDU_RESOURCE_H
+#define DESCENDU_RESOURCE_H
+
+namespace descendu {
+
+class resource
+{
+public:
+
+};
+
+} // namespace
+
+#endif
---
+++ test/resource.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2016 Rhys Ulerich
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <descendu/resource.hpp>
+
+using namespace descendu;
+
+TEST_CASE( "placeholder" ) {
+
+    SECTION( "placeholder" ) {
+        REQUIRE( 1 == 1 );
+    }
+
+}
