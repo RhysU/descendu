c58ebe9610988824aa3bd6346f0cf234efc2103c
Rhys Ulerich <rhys.ulerich@gmail.com>
2017-01-26 20:50:59 -0500

    Stub s-expression parser + test

---
+++ descendu/sexp.hpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 Rhys Ulerich
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef DESCENDU_SEXP_HPP
+#define DESCENDU_SEXP_HPP
+
+namespace descendu {
+
+} // namespace
+
+#endif
