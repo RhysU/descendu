9536d66df5f374f39aa9adbf60e0a35ae547c228
Rhys Ulerich <rhys.ulerich@gmail.com>
2017-01-26 21:08:32 -0500

    Stub s expression algorithm

--- descendu/sexp.hpp
+++ descendu/sexp.hpp
@@ -9,7 +9,61 @@
 #ifndef DESCENDU_SEXP_HPP
 #define DESCENDU_SEXP_HPP
 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iterator>
+
+#include "optional.hpp"
+
 namespace descendu {
+
+namespace sexp {
+
+//// Saith https://en.wikipedia.org/wiki/S-expression#Parsing
+//// regarding a particularly cute algorithm.  Who am I to argue?
+// def parse_sexp(string):
+//     """
+//     >>> parse_sexp("(+ 5 (+ 3 5))")
+//     [['+', '5', ['+', '3', '5']]]
+//
+//     """
+//     sexp = [[]]
+//     word = ''
+//     in_str = False
+//     for char in string:
+//         if char == '(' and not in_str:
+//             sexp.append([])
+//         elif char == ')' and not in_str:
+//             if word:
+//                 sexp[-1].append(word)
+//                 word = ''
+//             temp = sexp.pop()
+//             sexp[-1].append(temp)
+//         elif char in (' ', '\n', '\t') and not in_str:
+//             if word:
+//                 sexp[-1].append(word)
+//                 word = ''
+//         elif char == '\"':
+//             in_str = not in_str
+//         else:
+//             word += char
+//     return sexp[0]
+
+struct node {
+    std::experimental::optional<std::string> string;
+    std::vector<node> children;
+};
+
+template<typename CharT, class Traits>
+std::vector<node> parse(std::basic_istream<CharT,Traits>& is) {
+    std::vector<node> sexp;
+    // TODO
+    return sexp;
+}
+
+
+} // namespace
 
 } // namespace
 
