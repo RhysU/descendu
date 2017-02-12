6497e014a46d538c9a7d51ad035ca6400d2df558
Rhys Ulerich <rhys.ulerich@gmail.com>
2017-02-11 21:49:55 -0500

    Stub test/trial

---
+++ test/trial.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2017 Rhys Ulerich
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <sstream>
+
+#include <descendu/world.hpp>
+
+using namespace descendu;
+
+int main(const int argc, const char * argv[]) {
+    // TODO
+    return 0;
+}
